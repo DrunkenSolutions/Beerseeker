9e39b02bd2c472825471c5f083dc99d40df08ad6
Dmytro Rymar <rymar.dmytro@gmail.com>
2016-03-13 12:23:48 +0200

    map API key added

--- app/src/main/java/com/dmytryk/beerseeker/MainActivity.java
+++ app/src/main/java/com/dmytryk/beerseeker/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -46,8 +47,9 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
             }
         });
 
@@ -160,6 +162,6 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-
+        Toast.makeText(this, "Connection failed", Toast.LENGTH_LONG).show();
     }
 }
