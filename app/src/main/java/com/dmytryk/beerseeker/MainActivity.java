aa0d7cdcabed8b69c88c9b2c3cf23f3ae797c6da
Dmytro Rymar <rymar.dmytro@gmail.com>
2016-03-13 12:24:52 +0200

    warning fixed

--- app/src/main/java/com/dmytryk/beerseeker/MainActivity.java
+++ app/src/main/java/com/dmytryk/beerseeker/MainActivity.java
@@ -7,6 +7,7 @@ import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -161,7 +162,7 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
         Toast.makeText(this, "Connection failed", Toast.LENGTH_LONG).show();
     }
 }
